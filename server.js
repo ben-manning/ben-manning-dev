e3858a2fa44c997645e43a64ed33a55fc704c874
Ben Manning <bmanning@me.com>
2015-07-27 14:19:57 -0700

    set route for the index page

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+//=============== END OF BOILERPLATE ===================
+
+
+// using bodyParser middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/public'));
+app.listen(3000, function() { console.log('listening')});
+
+//=============== END OF RUNNING SERVER ================
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-})
+  res.sendFile(__dirname + '/views/index.html');
+});
 
-var server = app.listen(3000);
+
